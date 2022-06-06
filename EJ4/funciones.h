998ac93efddd08c19254f796abfb4070d82c9aca
Enzo Belmonte <ebelmonte@frba.utn.edu.ar>
2022-06-06 16:31:21 -0300

    Ej4

---
+++ EJ4/funciones.h
@@ -0,0 +1,22 @@
+/*!
+ * @file    funciones.h
+ * @brief   Implementar una función que compute el promedio de dos números enteros y almacene el resultado de la operación en una variable
+ *          pasada por referencia.
+ * @author  Enzo Belmonte <ebelmonte@frba.utn.edu.ar>
+ * @date    03/06/2022
+ */
+#ifndef FUNCIONES_H_
+#define FUNCIONES_H_
+
+
+/*!
+ * @brief Función que compute el promedio de dos números enteros.
+ *
+ * @param[in] int x, int y Variables de entrada.
+ * @return	  -
+ */
+void promedio(int, int, float*);
+
+
+
+#endif // FUNCIONES_H_
